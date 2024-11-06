0c1c912b982c41d86694c7b365724804e876c3d8
stuart townsend <stubet17@outlook.com>
2024-11-06 20:34:30 +0000

    add current-user format nav links according to user login state

--- dcdirect/views.py
+++ dcdirect/views.py
@@ -8,27 +8,30 @@ views = Blueprint('views', __name__)
 
 @views.route("/")
 def home():
-    return render_template("events.html")
+    return render_template("events.html", user=current_user)
 
 @views.route("/places")
 def places():
-    return render_template("places.html")  
+    return render_template("places.html", user=current_user)  
 
 @views.route("/fooddrink")
 def fooddrink():
-    return render_template("fooddrink.html")
+    return render_template("fooddrink.html", user=current_user)
 
 @views.route("/myplaces")
+@login_required
 def myplaces():
-    return render_template("myplaces.html")
+    return render_template("myplaces.html", user=current_user)
 
 @views.route("/myevents")
+@login_required
 def myevents():
-    return render_template("myevents.html")
+    return render_template("myevents.html", user=current_user)
 
 @views.route("/myfood")
+@login_required
 def myfood():
-    return render_template("myfood.html")
+    return render_template("myfood.html", user=current_user)
 
 
     
